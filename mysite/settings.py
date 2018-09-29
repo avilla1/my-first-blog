a47027c4d9a893ea019dbb9b4bb3b4555eb45114
Your Name <you@example.com>
2018-09-29 11:57:21 -0700

    update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x@@a$wcw&@mf5r^^vf+)v34%164$&+zl#$l0&it_fg&cmqcdkq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
